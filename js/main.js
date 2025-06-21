dec66e420df20ed6ccd877bd98896bb130849fd8
Conrado Peloso <conradopeloso.cp@gmail.com>
2025-06-21 12:18:31 -0300

    visualizacion de productos en pantalla principal

--- js/main.js
+++ js/main.js
@@ -41,3 +41,39 @@ btnSesion.addEventListener("click", () => {
 btnUsuario.addEventListener("click", () => {
     window.location.href = "/pages/datos_usuario.html";
 });
+
+// MUESTRA DE PRODUCTOS EN LA PAGINA PRINCIPAL
+document.addEventListener("DOMContentLoaded", () => {
+    actualizarIconoSesion();
+    cargarProductosEnPagina();
+});
+
+async function cargarProductosEnPagina() {
+    try {
+        const res = await fetch('http://localhost:4000/api/obtenerProductos');
+        const data = await res.json();
+        const productos = data.payload[0]; // asumiendo la estructura que usás
+
+        const contenedor = document.getElementById('productos-lista');
+        contenedor.innerHTML = ''; // limpiar
+
+        productos.forEach(prod => {
+            const tarjeta = document.createElement('div');
+            tarjeta.classList.add('tarjeta');
+
+            tarjeta.innerHTML = `
+                <img src="${prod.imagen || prod.ulrImagen || ''}" alt="${prod.producto}">
+                <div class="descripcion">
+                    <strong class= "nombre-producto">${prod.producto}</strong><br>
+                    ${prod.descripcion}<br>
+                    <small>$${prod.precio}</small><br>
+                    <small><em>Categoría: ${prod.categoria}</em></small>
+                </div>
+            `;
+
+            contenedor.appendChild(tarjeta);
+        });
+    } catch (error) {
+        console.error('Error al cargar productos para la página:', error);
+    }
+}
